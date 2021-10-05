18388d6bfa864c58cb42dbef57ab68677bc95bd1
chan.park <chan.park@life.church>
2021-10-04 20:19:33 -0500

    Add new files

---
+++ src/templates/navbar.js
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+import PageButton from "../components/pageButton";
+
+const Navbar = ({ page, pages, setPage }) => {
+  const styling = {
+    background: "#eaaaaa",
+    justifyContent: "space-evenly",
+    padding: "20px",
+    width: "100%",
+  };
+
+  return (
+    <div style={styling}>
+      {pages.map((p) => (
+        <PageButton key={p} onClick={() => setPage(p)} selected={p === page}>
+          {p[0].toUpperCase() + p.substring(1)}
+        </PageButton>
+      ))}
+    </div>
+  );
+};
+
+export default Navbar;
